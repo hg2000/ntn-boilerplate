37b30bfad8e094f596d723259fe9a7088438bec5
Hendrik Göbel <hendrik.goebel@cn-consult.eu>
2023-01-02 16:46:48 +0100

    clean up

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,29 @@
 <template>
   <main>
-    <section class="self-center flex flex-col flex-1 items-center justify-center">
-      <h1 class="title text-center">Nuxt — Tailwind — Netlify CMS</h1>
-      <h2 class="subtitle text-center">Boilerplate</h2>
-    </section>
+    <section v-if="posts" class="w-full max-w-5xl mx-auto">
+      <h1 class="title">Changelogs</h1>
 
-    <section class="mt-8">
-      <h3 class="text-primary-600 dark:text-primary-400 max-w-5xl mx-auto">Latest blog post</h3>
-      <posts post-type="blog" :amount="1" />
+      <ul>
+        <li v-for="item in posts">
+          {{ item.title }}
+        </li>
+      </ul>
+
+
     </section>
   </main>
 </template>
+
+<script>
+export default {
+  async asyncData({$content, error}) {
+    let posts;
+    try {
+      posts = await $content("changelogs").fetch();
+    } catch (e) {
+      error({message: "Projects not found"});
+    }
+    return {posts};
+  },
+}
+</script>
